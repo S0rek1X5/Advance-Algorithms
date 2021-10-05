072eea362e4ea8d7ee9aff905881760f84e389d5
Daniel <danielbebon@gmail.com>
2021-10-04 19:55:21 -0500

    Update Calculator.c

--- Case-1/Calculator.c
+++ Case-1/Calculator.c
@@ -5,13 +5,13 @@ int main()
 	int opc1,opc2,i,cant1,cant2;
 	char cf[13];
 	do{
-		printf("------Bienvenido------\nIntroduzca sus datos\n\n");
-		printf("Digite la cantidad de estudiantes VARONES: ");scanf("%d",&cant1);
-		printf("Digite la cantidad de estudiantes MUJERES: ");scanf("%d",&cant2);
+		printf("------WELCOME------\nIntroduce your data\n\n");
+		printf("Type the amount of boy students: ");scanf("%d",&cant1);
+		printf("Type the amount of girl students: ");scanf("%d",&cant2);
 		printf("------\n");
-		printf("Chicos\n");
+		printf("Boys\n");
 		for(i=0;i<cant1;i++){
-			printf("Digite la estatura del estudiante %i: ",i+1);scanf("%f",&chico);
+			printf("Type the stature of the student %i: ",i+1);scanf("%f",&chico);
 			sumaB += chico;
 			promB=sumaB/cant1;
 			if(chico>mayB){
@@ -19,9 +19,9 @@ int main()
 			}
 		}
 		printf("------\n");
-		printf("Chicas\n");
+		printf("Girls\n");
 		for(i=0;i<cant2;i++){
-			printf("Digite la estatura de la estudiante %i: ",i+1);scanf("%f",&chica);
+			printf("Type the stature of the student %i: ",i+1);scanf("%f",&chica);
 			sumaG += chica;
 			promG=sumaG/cant2;
 			if(chica>mayG){
@@ -34,38 +34,38 @@ int main()
 			mayAll=mayG;
 		}
 		do{
-			printf("\nQue desea saber?");
-			printf("\nA---Estatura mas alta\nB---Estatura mas alta CHICO\nC---Estatura mas alta CHICA\nD---Promedio de estatura de todos\nE---Promedio de estatura de chicos\nF---Promedio de estatura de chicas\nG---Todas las anteriores\n: ");scanf("%s",&cf);
+			printf("\nWhat you want to know?");
+			printf("\nA---Taller stature\nB---Taller stature BOY\nC---Taller stature Girl\nD---Average stature of all\nE---Average stature of boys\nF---Average stature of girls\nG---All of the aboves\n: ");scanf("%s",&cf);
 			if(cf[0]=='A' || cf[0]=='a'){
-				printf("\nLa estatura mas alta es: %.2f", mayAll);
+				printf("\nThe tallest stature is: %.2f", mayAll);
 			}
 			else if(cf[0]=='B' || cf[0]=='b'){
-				printf("\nLa estatura mas alta entre los chicos es: %.2f", mayB);
+				printf("\nThe tallest stature among boys is: %.2f", mayB);
 			}
 			else if(cf[0]=='C' || cf[0]=='c'){
-				printf("\nLa estatura mas alta entre las chicas es: %.2f", mayG);
+				printf("\nThe tallest stature among girls is: %.2f", mayG);
 			}
 			else if(cf[0]=='D' || cf[0]=='d'){
-				printf("\nEl promedio de estatura de todos es: %.2f", promAll);
+				printf("\nThe average stature of all is: %.2f", promAll);
 			}
 			else if(cf[0]=='E' || cf[0]=='e'){
-				printf("\nEl promedio de estatura de los chicos es: %.2f", promB);
+				printf("\nThe average stature of boys is: %.2f", promB);
 			}
 			else if(cf[0]=='F' || cf[0]=='f'){
-				printf("\nEl promedio de estatura de las chicas es: %.2f", promG);
+				printf("\nThe average stature of girls is: %.2f", promG);
 			}
 			else if(cf[0]=='G' || cf[0]=='g'){
-				printf("\nLa estatura mas alta es: %.2f", mayAll);
-				printf("\nLa estatura mas alta entre los chicos es: %.2f", mayB);
-				printf("\nLa estatura mas alta entre las chicas es: %.2f", mayG);
-				printf("\nEl promedio de estatura de todos es: %.2f", promAll);
-				printf("\nEl promedio de estatura de los chicos es: %.2f", promB);
-				printf("\nEl promedio de estatura de las chicas es: %.2f", promG);
+				printf("\nThe tallest stature is: %.2f", mayAll);
+				printf("\nThe tallest stature among boys is: %.2f", mayB);
+				printf("\nThe tallest stature among girls is: %.2f", mayG);
+				printf("\nThe average stature of all is: %.2f", promAll);
+				printf("\nThe average stature of boys is: %.2f", promB);
+				printf("\nThe average stature of girls is: %.2f", promG);
 			}
-			printf("\nMarque 1 para CONTINUAR --- Marque 2 para SALIR: ");scanf("%d",&opc2);
+			printf("\nType 1 to CONTINUE --- Type 2 to EXIT: ");scanf("%d",&opc2);
 			printf("\n----------------------------------\n");
 		}while(opc2==1);
-		printf("\nMarque 1 para REINTENTAR --- Marque 2 para SALIR: ");scanf("%d",&opc1);
+		printf("\nType 1 to RETRY --- Type 2 to EXIT: ");scanf("%d",&opc1);
 		printf("\n----------------------------------\n");
 	}while(opc1==1);
 	return 0;
