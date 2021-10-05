3380b809727fd61dbbab533ff9c9e0788be895fe
Daniel <danielbebon@gmail.com>
2021-10-04 19:55:26 -0500

    Update Calculator.c

--- Case-2/Calculator.c
+++ Case-2/Calculator.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main ()
 {
-	float add=0,promediot,max=0,min,porA,porB,porC,porD,porF,estud;
+	float add=0,promediot,max=0,min=999,porA,porB,porC,porD,porF,estud;
 	int i,cant,opc,A=0,B=0,C=0,D=0,F=0;
 	do{
-		printf("------Bienvenido------\n");
-		printf("Cuantos estudiantes conforman su grupo?: ");scanf("%i",&cant);
+		printf("------Welcome------\n");
+		printf("How much students make up your group?: ");scanf("%i",&cant);
 		for(i=0;i<cant;i++){
-			printf("\nIntroduzca la calificacion del estudiante %i\n: ",i+1);scanf("%f",&estud);
+			printf("\nEnter the student's grade %i\n: ",i+1);scanf("%f",&estud);
 			add += estud;
 			promediot=add/cant;
 			if(estud>max){
@@ -33,13 +33,13 @@ int main ()
 			}	
 		}
 		porA=(A*100)/cant;porB=(B*100)/cant;porC=(C*100)/cant;porD=(D*100)/cant;porF=(F*100)/cant;
-		printf("\nEl promedio de sus estudiantes es de: %1.1f",promediot);
-		printf("\nCalificacion mas alta-> %1.1f\nCalificacion mas baja-> %1.1f\n",max,min);
+		printf("\nThe average of his students is: %1.1f",promediot);
+		printf("\nHighest rating-> %1.1f\nLowest rating-> %1.1f\n",max,min);
 		printf("--------------");
-		printf("\nEstadisticas\n");
-		printf("%i Sacanon A - %i Sacaron B\n%i Sacaron C - %i Sacaron D\n%i Sacaron F",A,B,C,D,F);
-		printf("\n\n%1.1f Saco A %1.1f Saco B %1.1f Saco C %1.1f Saco D %1.1f Saco F\n",porA,porB,porC,porD,porF);
-		printf("\nPresione 1 para REINTENTAR | Presione 2 para SALIR\n: ");scanf("%d",&opc);
+		printf("\nstatistics\n");
+		printf("%i Won A - %i Won B\n%i Won C - %i Won D\n%i Won F",A,B,C,D,F);
+		printf("\n\n%1.1f Won A %1.1f Won B %1.1f Won C %1.1f Won D %1.1f Won F\n",porA,porB,porC,porD,porF);
+		printf("\nPress 1 To CONTINUE | Press 2 to EXIT\n: ");scanf("%d",&opc);
 		printf("\n--------------------------------------\n");
 	}while(opc==1);
 	return 0;
